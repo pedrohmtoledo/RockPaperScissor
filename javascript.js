c467f9154333652287226f7ea1a494419a2a033d
pedrohmtoledo <toledopedro.hmt@gmail.com>
2024-09-13 19:53:56 +0100

    Finished rock paper scissor with playGame function that runs the game 5 times and messages the score and the winner if any.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,33 @@
-let humanScore = 0;
-let computerScore = 0;
 
+// This function will randomize the computer's choice
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0){
-        computerChoice = "paper";
+        computerChoice = "paper"
     }
     else if (randomNumber === 1){
-        computerChoice = "rock";
+        computerChoice = "rock"
     }
     else {
-        computerChoice = "scissor";
+        computerChoice = "scissor"
     }
-    return computerChoice
-    console.log(computerChoice)    
+    return computerChoice;
+    console.log(computerChoice);
 }
 
-
+// Function will prompt the user for a choice
 function getHumanChoice() {
     humanChoice = prompt("Rock, paper or scissor?")
-    return humanChoice
-    console.log(humanChoice)
+    return humanChoice;
+    console.log(humanChoice);
 }
 
 
+// A round function that takes computer and human choices and gets the winner
 function playRound(humanChoice, computerChoice){
+    let humanScore = 0;    //Variable to store the human score
+    let computerScore = 0;  //Variable to store the computer score
+
     if (humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper"){
         message = "You lose. Paper beats rock!!"
         computerScore++
@@ -52,11 +55,41 @@ function playRound(humanChoice, computerChoice){
     else {
         message = "It's a tie"
     }
-    console.log(message)
-    console.log(`Your score is: ${humanScore}\n Computer score is ${computerScore}`)
+     
+    console.log(message);
+    console.log(`Your score is: ${humanScore}\n Computer score is ${computerScore}`);
+    return [humanScore, computerScore]
+
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection)
+
+// Function game that will play 5 rounds of rock, paper scissor
+function playGame() {
+    let humanScr = 0;
+    let computerScr = 0;
+    for (let i = 0; i < 5; i++){
+        // Variables that stores human and computer choice
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        let result = playRound(humanSelection, computerSelection)
+        humanScr+= result[0]
+        computerScr+= result[1]
+    }
+    let score = `Final score is Human ${humanScr} x ${computerScr} Computer`
+    if (humanScr > computerScr){
+        message = "Congratulations you won!"
+    }
+    else if (humanScr < computerScr){
+        message = "You lost =(."
+    }
+    else {
+        message = "Game was a tie."
+    }
+    console.log(score)
+    console.log(message)
+
+}
+
+playGame()
+
